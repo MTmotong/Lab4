1ea5081a3ec8ccc1bed60d40e921839be742d363
MTmotong <dzymt6627@163.com>
2016-10-18 19:05:25 +0800

    modify with PMD

--- src/yunsuan/Yun.java
+++ src/yunsuan/Yun.java
@@ -1,170 +1,178 @@
 package yunsuan;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * @author dzy
+ * @author dzy.
  *
  */
 
 public final class Yun {
+    static final Logger log = Logger.getLogger(Yun.class.getName());
 
 /**
-*
+*.
  */
 /**
-*
+*.
  */
-private Yun() {
-}
+  private Yun() {
+  }
 
-/**
- * @param args
- */
+    /**
+     * @param args.
+     */
 
 
-/**
+    /**
     * huihuihefiuw.
 
     * @param args dnejwf
     */
-    public static void main(final String[] args) {
-/*
-*
-*/
-// TODO Auto-generated method stub
-        final Scanner in = new Scanner(System.in);
-        System.out.println("请输入多项式：");
-        String abs;
-        int c = 1;
-        abs = in.nextLine();
-        final int b = abs.length();
-        final char[] a1 = new char[b];
-        final char[] c1 = new char[b];
-        for (int i = 0; i < b; i++) {
-            a1[i] = abs.charAt(i);
-            c1[i] = abs.charAt(i);
-        }
-        for (int i = 0; i < b; i++) {
-            if (i < b - 1 && a1[i] != '+' && a1[i] != '*') {
-                if (a1[i + 1] != '+' && a1[i + 1] != '*') {
+  public static void main(final String[] args) {
+    /*
+    *
+    */
+    // TODO Auto-generated method stub
+    final Scanner in = new Scanner(System.in);
+    System.out.println("请输入多项式：");
+    String abs;
+    int c = 1;
+    abs = in.nextLine();
+    final int b = abs.length();
+    final char[] a1 = new char[b];
+    final char[] c1 = new char[b];
+    for (int i = 0; i < b; i++) {
+        a1[i] = abs.charAt(i);
+        c1[i] = abs.charAt(i);
+    }
+    for (int i = 0; i < b; i++) {
+        if (i < b - 1 && a1[i] != '+' && a1[i] != '*') {
+            if (a1[i + 1] != '+') {
+                if(a1[i + 1] != '*') {
                     System.out.println("输入错误");
                     c = 0;
                     break;
+              	}
+            }
+        }
+    }
+    for (int i = 0; i < b; i++) {
+        final String l1 = String.valueOf(a1[i]);
+        final Pattern p = Pattern.compile("[a-zA-Z]");
+        final Pattern p2 = Pattern.compile("[0-9]");
+        final Pattern p3 = Pattern.compile("[*]");
+        final Pattern p4 = Pattern.compile("[+]");
+        final Matcher m1 = p.matcher(l1);
+        final Matcher m2 = p2.matcher(l1);
+        final Matcher m3 = p3.matcher(l1);
+        final Matcher m4 = p4.matcher(l1);
+        if (c == 1 && !m1.find()
+                && !m2.find() && !m3.find()
+                    && !m4.find()) {
+            System.out.println("输入错误");
+            break;
+        }
+        if (c == 1 && i == b - 1) {
+            System.out.println(abs);
+        }
+    }
+    System.out.println("请输入未知数的值：");
+    final String zhi = in.nextLine();
+    int plan = 0;
+    int g = 0;
+    final int f = zhi.length();
+    final char[] b1 = new char[f];
+    for (int i = 0; i < f; i++) {
+        b1[i] = zhi.charAt(i);
+    }
+    final String[] sss = zhi.split(" ");
+    int u = 0;
+    final int n1 = 9;
+    final int n2 = 10;
+    final int n3 = 12;
+    final int n4 = 4;
+    if (!sss[0].equals("!simplify")) {
+        System.out.println("输入错误");
+    } else {
+        if (f == n1) {
+            System.out.println(abs);
+        } else {
+            for (int i = 0; i < b; i++) {
+                if (a1[i] == b1[n2]) {
+                    a1[i] = b1[n3];
                 }
             }
-        }
-        for (int i = 0; i < b; i++) {
-            final String l1 = String.valueOf(a1[i]);
-            final Pattern p = Pattern.compile("[a-zA-Z]");
-            final Pattern p2 = Pattern.compile("[0-9]");
-            final Pattern p3 = Pattern.compile("[*]");
-            final Pattern p4 = Pattern.compile("[+]");
-            final Matcher m1 = p.matcher(l1);
-            final Matcher m2 = p2.matcher(l1);
-            final Matcher m3 = p3.matcher(l1);
-            final Matcher m4 = p4.matcher(l1);
-            if (c == 1 && !m1.find()
-                    && !m2.find() && !m3.find()
-                        && !m4.find()) {
-                System.out.println("输入错误");
-                break;
+            while (f - n3 - u > 1) {
+                u = u + n4;
+                for (int i = 0; i < b; i++) {
+                    if (a1[i] == b1[n2 + u]) {
+                        a1[i] = b1[n3 + u];
+                    }
+                }
             }
-            if (c == 1 && i == b - 1) {
-                System.out.println(abs);
+            for (int j = 0; j < b; j++) {
+                System.out.print(a1[j]);
             }
+            System.out.print("\n");
         }
-        System.out.println("请输入未知数的值：");
-        final String zhi = in.nextLine();
-        int plan = 0;
-        int g = 0;
-        final int f = zhi.length();
-        final char[] b1 = new char[f];
-        for (int i = 0; i < f; i++) {
-            b1[i] = zhi.charAt(i);
-        }
-        final String[] sss = zhi.split(" ");
-        int u = 0;
-        final int n1 = 9;
-        final int n2 = 10;
-        final int n3 = 12;
-        final int n4 = 4;
-        if (!sss[0].equals("!simplify")) {
+    }
+    //System.out.println("请输入要进行的求导：");
+    log.fine("请输入要进行的求导：");
+    final int n5 = 5;
+    final String dp = in.nextLine();
+    final char[] q1 = new char[n5];
+    int mm = 0;
+    //String rt = "";
+    final StringBuilder rt = new StringBuilder("");
+    for (int i = 0; i < n5; i++) {
+      q1[i] = dp.charAt(i);
+    }
+    for (int i = 0; i < b; i++) {
+        if (q1[0] != '!' || q1[1] != 'd' || q1[2] != '/') {
+            System.out.println("输入错误");
+            break;
+        } else if (i == b - 1 && c1[i] != q1[n4]) {
             System.out.println("输入错误");
         } else {
-            if (f == n1) {
-                System.out.println(abs);
-            } else {
-                for (int i = 0; i < b; i++) {
-                    if (a1[i] == b1[n2]) {
-                        a1[i] = b1[n3];
-                    }
-                }
-                while (f - n3 - u > 1) {
-                    u = u + n4;
-                    for (int i = 0; i < b; i++) {
-                        if (a1[i] == b1[n2 + u]) {
-                            a1[i] = b1[n3 + u];
+            while (mm < b) {
+                if (c1[mm] == q1[n4]) {
+                    g = mm;
+                    while (g < b - 1
+                            && (c1[g] == q1[n4]
+                                    || c1[g] == '*'
+                                        || c1[g + 1] == '*')) {
+                        if (c1[g] == q1[n4]) {
+                            plan = plan + 1;
                         }
+                        g = g + 1;
                     }
                 }
-                for (int j = 0; j < b; j++) {
-                    System.out.print(a1[j]);
+                if (plan == 0) {
+                    //rt = rt + c1[mm];
+                    rt.append(c1[mm]);
+                    mm = mm + 1;
+                } else {
+                    final String str = String
+                        .valueOf(plan);
+                    //rt = rt + str;
+                    rt.append(str);
+                    int j;
+                    for (j = mm + 1; j < g + 1; j++) {
+                        //rt = rt + c1[j];
+                        rt.append(c1[j]);
+                    }
+                    mm = g + 1;
+                    plan = 0;
                 }
-                System.out.print("\n");
             }
+            System.out.println(rt);
+            break;
         }
-        System.out.println("请输入要进行的求导：");
-        final int n5 = 5;
-        final String dp = in.nextLine();
-        final char[] q1 = new char[n5];
-        int mm = 0;
-        String rt = "";
-        for (int i = 0; i < n5; i++) {
-            q1[i] = dp.charAt(i);
-        }
-        for (int i = 0; i < b; i++) {
-            if (q1[0] != '!' || q1[1] != 'd' || q1[2] != '/') {
-                System.out.println("输入错误");
-                break;
-            } else if (i == b - 1 && c1[i] != q1[n4]) {
-                System.out.println("输入错误");
-            } else {
-                while (mm < b) {
-                    if (c1[mm] == q1[n4]) {
-                        g = mm;
-                        while (g < b - 1
-                                && (c1[g] == q1[n4]
-                                        || c1[g] == '*'
-                                            || c1[g + 1] == '*')) {
-                            if (c1[g] == q1[n4]) {
-                                plan = plan + 1;
-                            }
-                            g = g + 1;
-                        }
-                    }
-                    if (plan == 0) {
-                        rt = rt + c1[mm];
-                        mm = mm + 1;
-                    } else {
-                        final String str = String
-                            .valueOf(plan);
-                        rt = rt + str;
-                        int j;
-                        for (j = mm + 1; j < g + 1; j++) {
-                            rt = rt + c1[j];
-                        }
-                        mm = g + 1;
-                        plan = 0;
-                    }
-                }
-                System.out.println(rt);
-                break;
-            }
-        }
-        in.close();
     }
-
+    in.close();
+  }
 }
